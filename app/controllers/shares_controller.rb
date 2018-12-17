acc57a46d24e716c28579ee8fd98c86e0eda1f27
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-12-17 22:39:33 +0100

    adding filter by curency

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,11 +1,12 @@
 class SharesController < ApplicationController
   def index
     puts params[:currency_query]
-
+    puts params[:currency_query].empty?
     if params[:query].present?
-      sql_query = "isin ILIKE :query OR currencyspecificisin ILIKE :query OR fundname ILIKE :query"
-
-      @shares = Share.where(sql_query, query: "%#{params[:query]}%")
+      sql_query = "(isin ILIKE :query OR currencyspecificisin ILIKE :query OR fundname ILIKE :query)"
+        sql_query += " AND currencyid= :currency_query" if !params[:currency_query].empty?
+      @shares = Share.where(sql_query, query: "%#{params[:query]}%",
+                                       currency_query: "#{params[:currency_query]}")
       @isin_search = params[:query]
     else
       @shares = []
