36f0067077910f2738d4456a60ad6b699062abb6
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-12-17 23:03:21 +0100

    improving search share

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,10 +1,12 @@
 class SharesController < ApplicationController
   def index
-    puts params[:currency_query]
-    puts params[:currency_query].empty?
     if params[:query].present?
       sql_query = "(isin ILIKE :query OR currencyspecificisin ILIKE :query OR fundname ILIKE :query)"
-        sql_query += " AND currencyid= :currency_query" if !params[:currency_query].empty?
+      unless params[:currency_query].nil?
+        if !params[:currency_query].empty?
+          sql_query += " AND currencyid= :currency_query"
+        end
+      end
       @shares = Share.where(sql_query, query: "%#{params[:query]}%",
                                        currency_query: "#{params[:currency_query]}")
       @isin_search = params[:query]
