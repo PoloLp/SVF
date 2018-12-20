e0cb7bb3035a3d30baf2ff4bef348156e30dbacd
pa hocke <hocke.p.a@gmail.com>
2018-12-20 08:03:51 +0100

    created company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_180736) do
+ActiveRecord::Schema.define(version: 2018_12_19_172916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2018_12_18_180736) do
     t.string "typecode"
     t.string "definitionfrench"
     t.string "definition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
