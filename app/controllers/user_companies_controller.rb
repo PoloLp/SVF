fca1849db935f0f88f87de536579c5d7d6703f75
pa hocke <hocke.p.a@gmail.com>
2019-01-08 19:05:47 +0100

    listing all funds from companies list

--- app/controllers/user_companies_controller.rb
+++ app/controllers/user_companies_controller.rb
@@ -1,2 +1,19 @@
 class UserCompaniesController < ApplicationController
+  def index
+    @user = current_user
+    @companies = Company.all
+    @share_catalog = ShareCatalog.where(company_id: @companies.ids, status: true)
+    @share_catalog = @share_catalog.order('updated_at DESC')
+    @share_catalog_list = Share.where(id: share_ids_array(@share_catalog))
+  end
+
+  private
+
+  def share_ids_array(share_catalog)
+    sia = []
+    share_catalog.each do |s|
+      sia << s.share_id
+    end
+    return sia
+  end
 end
